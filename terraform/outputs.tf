c8b58533f5661bca0c7c4a9ab52f7e339479225d
Wes Lambert <wlambertts@gmail.com>
2020-04-23 14:14:13 +0000

    Refactor and other updates

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,6 +3,18 @@ output "Region" {
 }
 
 output "securityonion_public_ips" {
-  description = "Security Onion Training IP Addresses"
+  description = "Security Onion Public IP Address(es)"
   value = aws_instance.securityonion[*].public_ip
 }
+
+output "windows_public_ips" {
+  description = "Windows Instance IP Address(es)"
+  value = aws_instance.windows_instance[*].public_ip
+}
+
+output "ubuntu_public_ips" {
+  description = "Ubuntu Instance IP Address(es)"
+  value = aws_instance.ubuntu_instance[*].public_ip
+}
+
+
