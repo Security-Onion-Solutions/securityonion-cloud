607bee96e24aa6883a195bb0af9e6231425ddd7e
Jin Kim <jinwoov@gmail.com>
2020-10-23 22:57:53 -0500

    fixed it grabbing an object instead of the id(int value) in ubuntu.tf and windows.tf file, which was causing error

--- terraform/aws/windows.tf
+++ terraform/aws/windows.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "windows_instance" {
   depends_on = [ null_resource.mirror_session_del_wait, aws_lambda_function.auto_mirror_lambda ]
   count         = var.windows_hosts != 0 ? var.windows_hosts : 0
   instance_type = var.windows_instance_type
-  ami           = data.aws_ami.latest_windows.id != "" ? data.aws_ami.latest_windows : var.windows_instance_ami
+  ami           = data.aws_ami.latest_windows.id != "" ? data.aws_ami.latest_windows.id : var.windows_instance_ami
 
   tags = var.auto_mirror ? { Name = "windows-${count.index}", Mirror = "True" } : { Name = "windows-${count.index}" }
 
